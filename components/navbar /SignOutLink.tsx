c5298a4061bd6e56202349922bca9fd268cee21d
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-10-17 15:47:13 +0200

    👍 IMPROVE: Dropdown menu improved

--- components/navbar /SignOutLink.tsx
+++ components/navbar /SignOutLink.tsx
@@ -1,5 +1,23 @@
+'use client'
+
+import { SignOutButton } from "@clerk/nextjs"
+import { useToast } from "@/hooks/use-toast"
+
 export default function SignOutLink() {
+    const { toast } = useToast()
+
+    const handleLogout = () => {
+        toast({ description: "You have been logged out." })
+    }
+
     return (
-        <div className="text-3xl">SignOutLink</div>
+        <SignOutButton redirectUrl="/">
+            <button
+                className="w-full text-left"
+                onClick={handleLogout}
+            >
+                LogOut
+            </button>
+        </SignOutButton>
     )
 }
