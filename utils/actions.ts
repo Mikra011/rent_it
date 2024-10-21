affb4cc6d15453b947c5baf11da757370e0e5a64
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-10-21 12:12:31 +0200

    👍 IMPROVE: Create profile page improved

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,37 @@
 'use server'
 
 import { profileSchema } from "./schemas"
+import db from './db'
+import { auth, clerkClient, currentUser } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
 
-export const createProfileAction = async (prevState: any, formData: FormData) => {
+export const createProfileAction = async (
+    prevState: any,
+    formData: FormData
+) => {
     try {
+        const user = await currentUser()
+        if (!user) throw new Error('Please login to create a profile')
         const rawData = Object.fromEntries(formData)
         const validatedFields = profileSchema.parse(rawData)
-        console.log(validatedFields)
-        return { message: 'Profile created' }
-    } catch (error) {
-        console.log(error)
-        return { message: 'There was an error' }
+        await db.profile.create({
+            data: {
+                clerkId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                profileImage: user.imageUrl ?? '',
+                ...validatedFields,
+            },
+
+        })
+        await clerkClient.users.updateUserMetadata(user.id, {
+            privateMetadata: {
+                hasProfile: true,
+            }
+        })
+    } catch (error) { 
+        console.log(error.message)
+        return { message: error instanceof Error ? error.message : 'There is an error' }
     }
+    redirect('/')
 }
